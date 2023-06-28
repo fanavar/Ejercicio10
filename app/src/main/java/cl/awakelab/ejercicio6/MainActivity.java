24c0a3bc50dd621ceaa4dc10b7f8802659bc6346
Fabián <fanavar@alumnos.ubiobio.cl>
2023-06-27 20:41:09 -0400

    ejercicio 10: el ejercicio está completo!!

--- app/src/main/java/cl/awakelab/ejercicio6/MainActivity.java
+++ app/src/main/java/cl/awakelab/ejercicio6/MainActivity.java
@@ -1,47 +1,39 @@
 package cl.awakelab.ejercicio6;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import cl.awakelab.ejercicio6.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        EditText textName;
-        EditText textLastName;
-        EditText textMail;
-        EditText textPass;
+        setContentView(binding.getRoot());
 
-        textName = findViewById(R.id.textName);
-        textLastName = findViewById(R.id.textLastName);
-        textMail = findViewById(R.id.textMail);
-        textPass = findViewById(R.id.textPass);
 
-        Button btnCreate = findViewById(R.id.btnCreate);
-
-        btnCreate.setOnClickListener(new View.OnClickListener() {
+        binding.btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = textName.getText().toString();
-                String lastName = textLastName.getText().toString();
-                String mail = textMail.getText().toString();
-                String pass = textPass.getText().toString();
+                String name = binding.textName.getText().toString();
+                String lastName = binding.textLastName.getText().toString();
+                String mail = binding.textMail.getText().toString();
+                String pass = binding.textPass.getText().toString();
 
 
-                if(textName.getText().toString().isEmpty() || textLastName.getText().toString().isEmpty() || textMail.getText().toString().isEmpty()
-                        || textPass.getText().toString().isEmpty()   ){
+                if(name.isEmpty() || lastName.isEmpty() || mail.isEmpty()
+                        || pass.isEmpty()   ){
                     Toast.makeText(getBaseContext(), "faltan campos por completar ", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(!textMail.getText().toString().contains("@")){
+                if(!mail.contains("@")){
                     Toast.makeText(getBaseContext(), "campo correo invalido ", Toast.LENGTH_SHORT).show();
                     return;
                 }
